0b441e9eb260720c9cc514898c3370e7bb2dba61
Rakesh Kashyap <rakesh1988@gmail.com>
2022-06-23 22:26:17 +0800

    adding success activity

---
+++ app/src/main/java/com/hackathon/accesibility/PaymentSuccessActivity.kt
@@ -0,0 +1,13 @@
+package com.hackathon.accesibility
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentSuccessActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.payment_success_activity_layout)
+    }
+
+}
